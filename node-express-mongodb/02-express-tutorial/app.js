bd920dacac86f28bec0c131c7db6cbda05c74cc7
Jishan2610 <jishan4all@gmail.com>
2023-01-10 12:21:03 +0530

    first change in server

--- node-express-mongodb/02-express-tutorial/app.js
+++ node-express-mongodb/02-express-tutorial/app.js
@@ -1,9 +1,24 @@
 const http=require('http');
-
+const {readFileSync}=require('fs');
+const homePage=readFileSync('./index.html');
 const server=http.createServer((req,res)=>{
-    console.log('user hit the server');
-    res.writeHead(200,{'content-type':'text/html'});
-    res.write('<h1>home page</h1>');
-    res.end();
+    const url=req.url;
+    if(url=='/'){
+        res.writeHead(200,{'content-type':'text/html'});
+        res.write(homePage);
+        res.end();
+    }
+    else if(url=='/contact'){
+        res.writeHead(200,{'content-type':'text/html'});
+        res.write('<h1>contact me</h1>');
+        res.end();
+    }
+    else{
+        res.writeHead(404,{'content-type':'text/html'});
+        res.write('<h1>result not found</h1>');
+        res.end();
+    }
+    //console.log('user hit the server');
+    
 })
 server.listen(5000);
